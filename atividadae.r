bf07e1386c652505def2eb38ee7aaaff62736ec5
rafaelSchimas <rafael.ranaudo@sptech.school>
2023-12-15 10:45:00 -0300

    Arrumando os TXT

--- atividadae.r
+++ atividadae.r
@@ -1,36 +1,32 @@
-
 install.packages("syuzhet")
 library(syuzhet)
 
-episodioIV <- read.csv("~/Documentos/projeto-star-wars-1CCO/raw/SW_EpisodeIV_enUS.txt", sep="")
-episodioV <- read.csv("~/Documentos/projeto-star-wars-1CCO/raw/SW_EpisodeV_enUS.txt", sep="")
-episodioVI <- read.csv("~/Documentos/projeto-star-wars-1CCO/raw/SW_EpisodeVI_enUS.txt", sep="")
+caminho = getwd()
 
-todosEpisodios <- rbind(episodioIV,episodioV,episodioVI)
+episodioIV <- read.csv(paste(caminho, "/raw/SW_EpisodeIV_enUS.txt", sep = ""), sep="")
+episodioV <- read.csv(paste(caminho, "/raw/SW_EpisodeV_enUS.txt", sep = ""), sep="")
+episodioVI <- read.csv(paste(caminho, "/raw/SW_EpisodeVI_enUS.txt", sep = ""), sep="")
 
-View(episodioIV)
+todosEpisodios <- rbind(episodioIV,episodioV)
 
-todosPersonagens <- table(todosEpisodios$personagem)
+todosPersonagens <- table(todosEpisodios$character)
 
 df_pers <- data.frame(todosPersonagens)
 
 View(df_pers)
 
 df_pers <- df_pers[order(df_pers$Freq, decreasing = TRUE),]
-typeof(df_pers)
 
 pers_prin = df_pers$Var1[1:5]
 
-pers_prin
-
-df = subset.data.frame(todosEpisodios, personagem == pers_prin[1])
+df = subset.data.frame(todosEpisodios, character == pers_prin[1])
 2:length(pers_prin)
 
 for(i in 2:length(pers_prin)){
-  df <- rbind(df, subset.data.frame(todosEpisodios, personagem == pers_prin[i]))
+  df <- rbind(df, subset.data.frame(todosEpisodios, character == pers_prin[i]))
 }
 
-barplot(table(df$personagem))
+barplot(table(df$character))
 
 #DETECÇÂO DE EMOÇÔES
 
